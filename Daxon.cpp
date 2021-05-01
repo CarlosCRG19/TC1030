ae1e3779c47911b6f58febcc9a54170cb3ee9200
DavBot02 <davidlopezsth@gmail.com>
2021-05-01 18:51:28 -0500

    Funcion muestra clientes

--- Daxon.cpp
+++ Daxon.cpp
@@ -17,6 +17,20 @@ int Daxon::encuentraSucursal(string _sucursal){
     return index;
 }
 
+
+void Daxon::muestraClientes()
+{
+    cout << "\nLista de Clientes" << endl;
+    cout << "------------------" << endl;
+    for (int i = 0; i < numClientes; i++)
+    {
+        cout << i + 1 << ".";
+        clientes[i].print();
+    }
+    cout << endl;
+}
+
+
 void Daxon::transferirEmpleado(string _nombreEmpleado, string _sucursalOrigen, string _sucursalDestino){
     
 }
